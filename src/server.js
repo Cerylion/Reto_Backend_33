53ee9c3564bcd0a1d7793847a30d5a01d8284613
Cerylion <fullthrottle.moran@gmail.com>
2024-06-11 11:56:14 -0600

    JWT now implemented

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const usersRouter = require('./routes/users.routes')
-// const authRouter = require('./routes/auth.routes')
+const authRouter = require('./routes/auth.routes')
 
 const app = express()
 
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/users', usersRouter)
-// app.use('/auth', authRouter)
+app.use('/auth', authRouter)
 
 
 app.get('/', (request, response) => {
---
+++ src/usecases/auth.usecase.js
@@ -0,0 +1,27 @@
+const createError = require('http-errors')
+
+const Users = require('../models/users.model')
+const encrypt = require('../lib/encrypt')
+const jwt = require('../lib/jwt')
+
+async function login (email, password) {
+  const user = await Users.findOne({ email: email })
+
+  if (!user) {
+    throw createError(401, 'your user face is invalid')
+  }
+
+  const isPasswordValid = await encrypt.compare(password, user.password)
+
+  if (!isPasswordValid) {
+    throw createError(401, 'That is not the right password... stop trying to steal accounts fropm decent people')
+  }
+
+  const token = jwt.sign({ id: user._id})
+  return token
+
+}
+
+module.exports = {
+  login
+}
